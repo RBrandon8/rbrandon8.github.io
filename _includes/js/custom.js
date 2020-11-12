b32a647a697d1e2c309bddfa840db73c06a17bfb
RBrandon8 <rbrandon@gmail.com>
2020-11-11 20:21:05 -0700

    disable comments

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -1,8 +1,2 @@
-<script src="https://unpkg.com/commentbox.io/dist/commentBox.min.js"></script>
-
-const commentBox = require('commentbox.io');
-// or if using the CDN, it will be available as a global "commentBox" variable.
-
-commentBox('5738229097562112-proj');
 
 
